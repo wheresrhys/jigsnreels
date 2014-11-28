007191fa82586d5e21e7c7ef332567968c683c41
Rhys Evans <wheresrhys@gmail.com>
2014-11-28 23:35:00 +0000

    rendering abc

--- client/components/abc-viewer/view.js
+++ client/components/abc-viewer/view.js
@@ -2,6 +2,15 @@
 
 var swig = require('swig/index');
 var allTunes = require('../../collections/tunes');
+var ABCJS = require('abcjs');
+var abcConf = {
+    scale: 0.6,
+    paddingtop: 0,
+    paddingbottom: 0,
+    paddingright: 0,
+    paddingleft: 0
+}
+
 
 module.exports = require('../../scaffolding/view').extend({
     // tpl: require('./tpl.html'),
@@ -10,7 +19,8 @@ module.exports = require('../../scaffolding/view').extend({
         this.parentEl = opts.parentEl;
         this.tune = allTunes.filter(function (tune) {
             return tune.get('_id') == opts.tuneId;
-        })[0]
+        })[0];
+        this.snippetOnly = opts.snippetOnly
 
         this.render = this.render.bind(this);
         this.destroy = this.simpleDestroy.bind(this);
@@ -23,8 +33,49 @@ module.exports = require('../../scaffolding/view').extend({
     },
 
     render: function () {
-        this.renderToDom(this.tune.get('abc'), true);
-        return this;
+        this.parentEl.innerHTML = '<div></div>';
+        this.el = this.parentEl.firstElementChild;
+        var abc = this.snippetOnly ? this.getSnippet : this.tune.get('abc');
+        ABCJS.renderAbc(this.el, abc, {}, abcConf, {});  
+        // if (self.snippetOnly) {
+        //     snippetsStore.cacheScore(self.arrangement, self.el[0].innerHTML);
+        // } 
+        // return this;
+    },
+
+    // renderScore: function () {
+    //     var self = this;
+    //     this.abc = 'X:1' +
+    //     //'\nT:' + scoreGenerator.name + 
+    //     '\nM:' + this.scoreGenerator.meter + 
+    //     '\nL:1/8' + 
+    //     //'\nR:' + scoreGenerator.rhythm + 
+    //     '\nK:' + this.arrangement.root + this.scoreGenerator.mode + 
+    //     '\n' + this.abc;
+
+    //     setTimeout(function () {
+    //         ABCJS.renderAbc(self.el[0], self.abc, {}, self.conf, {});  
+
+             
+    //     }, 100);        
+    // }
+
+    getSnippet: function () {
+        // var self = this;
+
+        // this.el.addClass('score-snippet');
+
+        // snippetsStore.getCachedScore(this.arrangement, function (obj) {
+        //     self.el.html(obj.score);
+        //     self.el[0].style.width = self.el.find('svg')[0].getAttribute('width') + 'px';
+        // }, function () {
+        //     self.abc = self.abc.replace(/^\|*:?/, '');
+        //     self.abc = self.abc.split('|');
+        //     self.abc = self.abc.slice(0, (self.abc[0].length < 4 ? 4 : 3)).join('|');
+        //     self.conf.scale = 0.5;
+        //     self.conf.staffwidth = 450;
+        //     self.renderScore();
+        // });
     }
 
 });
@@ -34,10 +85,10 @@ module.exports = require('../../scaffolding/view').extend({
 
 // require('src/tune/services/score-snippets');
 
-// var ABCJS = require('abcjs'),
-//     snippetsStore;
 
-// var ScoreDrawer = function (scoreGenerator, snippetOnly, el) {
+    // snippetsStore;
+
+// var ScoreDrawer = function (abc, snippetOnly) {
 //     this.el = el;
 //     this.scoreGenerator = scoreGenerator;
 //     this.snippetOnly = snippetOnly;
@@ -57,13 +108,7 @@ module.exports = require('../../scaffolding/view').extend({
 //         }
 
 //         this.abc = this.arrangement.abc;
-//         this.conf = {
-//             scale: 0.6,
-//             paddingtop: 0,
-//             paddingbottom: 0,
-//             paddingright: 0,
-//             paddingleft: 0
-//         };
+//         this.;
 
         
 
@@ -92,24 +137,7 @@ module.exports = require('../../scaffolding/view').extend({
 //         });
 //     },
 
-//     renderScore: function () {
-//         var self = this;
-//         this.abc = 'X:1' +
-//         //'\nT:' + scoreGenerator.name + 
-//         '\nM:' + this.scoreGenerator.meter + 
-//         '\nL:1/8' + 
-//         //'\nR:' + scoreGenerator.rhythm + 
-//         '\nK:' + this.arrangement.root + this.scoreGenerator.mode + 
-//         '\n' + this.abc;
 
-//         setTimeout(function () {
-//             ABCJS.renderAbc(self.el[0], self.abc, {}, self.conf, {});  
-
-//             if (self.snippetOnly) {
-//                 snippetsStore.cacheScore(self.arrangement, self.el[0].innerHTML);
-//             }  
-//         }, 100);        
-//     }
 
 // };
 
