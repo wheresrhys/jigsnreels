b96ff824d3a3a1b782be50e1f8d4079577049c10
Rhys Evans <wheresrhys@gmail.com>
2014-12-18 18:59:40 +0000

    tempoatrraily rinstated old tune picker

--- client/data/collections/tunes.js
+++ client/data/collections/tunes.js
@@ -2,6 +2,7 @@ var Tunes = require('backbone-es6').Collection.extend({
 	name: 'tunes',
 	url: require('../../scaffolding/api').url('tunes'),
 	model: require('../models/tune'),
+	viewModel: require('../view-models/tunes'),
 	populate: function () {
 		return this.promise || (this.promise = this.fetch({parse: true}));
 	}
