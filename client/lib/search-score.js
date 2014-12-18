678e1a87e68079992684b555ecd1bb4a0305c290
Rhys Evans <wheresrhys@gmail.com>
2014-12-18 20:27:53 +0000

    getting rid of dependency on weakmap

--- client/lib/search-score.js
+++ client/lib/search-score.js
@@ -2,9 +2,9 @@
 
 var liquidMetal = require('liquidmetal');
 
-var analyze = function (string) {
+var analyze = function (query) {
 	var obj = {};
-	obj.term = string
+	obj.term = query
 		.replace(/\b[kK]\:([ABCDEFG][#b]?)?(maj|mix|dor|min|aeo|\+|-)?(\s|$)/, function ($0, root, mode) {
 			obj.key = {
 				root: root,
@@ -29,7 +29,9 @@ var analyze = function (string) {
 };
 
 var scoreForKey = function (model, keyCriteria) {
-	if (!keyCriteria) return 1;
+	if (!keyCriteria) {
+		return 1;
+	}
 	return model.get('keys').some(function (key) {
 		if (keyCriteria.modes) {
 			var root = keyCriteria.root || '';
@@ -43,33 +45,31 @@ var scoreForKey = function (model, keyCriteria) {
 }
 
 var scoreForRhythm = function (model, rhythmCriteria) {
-	if (!rhythmCriteria) return 1;
+	if (!rhythmCriteria) {
+		return 1;
+	}
 	return model.get('rhythms').some(function (rhythm) {
 		return rhythmCriteria.rhythm.toLowerCase() === rhythm.toLowerCase();
 	}) ? 1 : 0;
 }
 
-
 module.exports = {
 	sort: function (models, criteria) {
 
 		if (criteria.key || criteria.rhythm) {
-			var models = models.filter(function (model) {
+			models = models.filter(function (model) {
 				return scoreForKey(model, criteria.key) * scoreForRhythm(model, criteria.rhythm);
 			});
 		}
 
-		var stringMatchScores = new WeakMap(models.map(function (model) {
-			return [
-				model,
-				criteria.term ? liquidMetal.score(model.get('name'), criteria.term) : 1
-			];
-		}));
-
+		var hash = {};
+		models.forEach(function (model) {
+			hash[model.id] = criteria.term ? liquidMetal.score(model.get('name'), criteria.term) : 1;
+		});
 
 		return models.sort(function(model1, model2) {
-			var score1 = stringMatchScores.get(model1);
-			var score2 = stringMatchScores.get(model2);
+			var score1 = hash[model1.id];
+			var score2 = hash[model2.id];
 			return score1 === score2 ? 0 : score1 > score2 ? -1: 1
 		}).map(function (model) {
 			return model;
