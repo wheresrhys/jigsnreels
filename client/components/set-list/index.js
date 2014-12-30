00f56729e0dc753c560ec925c664caf8f3bdcd8d
Rhys Evans <wheresrhys@gmail.com>
2014-12-30 23:49:14 +0000

    search for sets

--- client/components/set-list/index.js
+++ client/components/set-list/index.js
@@ -1,4 +1,5 @@
 var SetView = require('../set');
+var SearchView = require('../search');
 
 module.exports = require('../../scaffolding/view').extend({
 	tpl: require('./tpl.html'),
@@ -10,18 +11,42 @@ module.exports = require('../../scaffolding/view').extend({
 		this.render = this.render.bind(this);
 		this.destroy = this.simpleDestroy.bind(this);
 		this.listenTo(this.sets, 'sync', this.render);
+		this.staticRender();
+	},
+
+	staticRender: function () {
+		this.renderToDom(this.swig.render(this.tpl), true);
+		this.listEl = this.el.querySelector('.set-list__list');
+		this.search = new SearchView(this.childOpts('search', {
+			limit: -1,
+			items: this.sets.models,
+			getSubjects: function (set) {
+				return set.getTunes().concat([{
+					get: function (key) {
+						return key === 'name' ? set.get('name') : []
+					}
+				}])
+			}
+		}));
+		this.listenTo(this.search, 'results', this.render)
+		this.listenTo(this.search, 'invalid', this.render)
 		this.render();
 	},
 
-	render: function () {
-		this.renderToDom(this.swig.render(this.tpl), true);
-		this.listEl = this.el.querySelector('.set-list__list');
+	render: function (results) {
 		var self = this;
-		this.sets.models.sort(function (m1, m2) {
+		// don't rerender when we're applying no filter to an already unfiltered list
+		if (!results && !this.filtered && this.listEl.innerHTML) {
+			return;
+		}
+		this.filtered = !!results;
+		this.listEl.innerHTML = '';
+		var sets = results || this.sets.models.sort(function (m1, m2) {
 			var m1l = m1.tunebooks().length;
 			var m2l = m2.tunebooks().length;
 			return (m1l === m2l) ? 0 : (m1l > m2l) ? 1 : -1;
-		}).forEach(function (model) {
+		});
+		sets.forEach(function (model) {
 			setTimeout(function () {
 				var setView = new SetView(self.childOpts(self.listEl, {
 					set: model
